9f80f9912da7c6e4a79e72cceb4131a365e3c82c
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-10-25 23:34:11 -0500

    Fix and add vcpkg and semi sidewalk detection.

--- quado/main.cpp
+++ quado/main.cpp
@@ -1,4 +1,3 @@
-
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
@@ -7,6 +6,8 @@
 
 using namespace cv;
 using namespace std;
+
+// USE https://github.com/Yuelong-Yu/ERFNet-Caffe FOR DATASETS
 
 // Function for car detection
 void detectCars(Mat& frame, CascadeClassifier& carCascade) {
@@ -20,11 +21,44 @@ void detectCars(Mat& frame, CascadeClassifier& carCascade) {
 	}
 }
 
+// TEMP
+Mat postProcess(const cv::Mat& output) {
+	// Thresholding to create a binary mask
+	cv::Mat mask;
+	cv::threshold(output, mask, 0.5, 255, cv::THRESH_BINARY);
+
+	// Clean up the mask using morphological operations
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+	cv::morphologyEx(mask, mask, cv::MORPH_OPEN, kernel);
+	cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel);
+
+	return mask;
+}
+
+// Function for side walk detection
+void detectSidewalk(Mat& frame, dnn::Net semanticSegmentationModel) {
+	Mat blob = dnn::blobFromImage(frame, 1.0, Size(1024, 512), Scalar(0, 0, 0), false, false);
+	semanticSegmentationModel.setInput(blob);
+
+	Mat output = semanticSegmentationModel.forward();
+
+	// Post-processing for sidewalk detection
+	Mat sidewalkMask = postProcess(output);
+
+	// Overlay the sidewalk mask on the original frame
+	Mat sidewalkDetection;
+	frame.copyTo(sidewalkDetection, sidewalkMask);
+}
+
 int main(int, char**)
 {
 	// Load the pre-trained Haar Cascade classifier for car detection.
 	CascadeClassifier carCascade;
-	carCascade.load("C:/Users/lando/Documents/quado/cars.xml");
+	carCascade.load("C:/Users/lando/source/repos/robot-ai/quado/cars.xml");
+
+	// Load a pre-trained model (ENet) for semantic segmentation
+	dnn::Net semanticSegmentationModel = dnn::readNet("C:/Users/lando/source/repos/robot-ai/quado/erfnet_cityscapes.prototxt",
+	"C:/Users/lando/source/repos/robot-ai/quado/erfnet_cityscapes.caffemodel");
 
 	//--- INITIALIZE VIDEOCAPTURE
 	VideoCapture capture;
@@ -33,7 +67,7 @@ int main(int, char**)
 	// cap.open(0);
 	// OR advance usage: select any API backend
 	int deviceID = 0; // 0 = open default camera
-	int apiID = cv::CAP_ANY; // 0 = autodetect default API
+	int apiID = CAP_ANY; // 0 = autodetect default API
 
 	// open selected camera using selected API
 	capture.open(deviceID, apiID);
@@ -54,8 +88,8 @@ int main(int, char**)
 			break;
 		}
 
-		// Detect cars
 		detectCars(frame, carCascade);
+		detectSidewalk(frame, semanticSegmentationModel);
 
 		// DEBUG: Shows the results of the masks and 
 		imshow("Live", frame);
